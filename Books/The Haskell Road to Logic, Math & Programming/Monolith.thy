e777701fab5a9fe3dbc087e6781f2d50cc82f1be
Josh Tilles <MerelyAPseudonym@gmail.com>
2014-01-27 20:18:26 -0500

    Add HR exercise 2.22 (Density of Rationals).

---
+++ Books/The Haskell Road to Logic, Math & Programming/Monolith.thy
@@ -0,0 +1,16 @@
+theory Monolith
+imports Main
+        Rat
+begin
+
+(* exercise 2.22 *)
+(* Between every two rational numbers there is a third *)
+lemma DensityOfRationals1:
+  fixes a b :: rat
+  shows "\<exists>(x::rat). a < x \<and> x < b"
+sorry
+
+lemma DensityOfRationals2:
+  fixes a b :: rat
+  obtains x where "a < x \<and> x < b"
+sorry
